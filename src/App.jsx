93c8e6d0a723b23df8bbe6dbbc7dc143b0519975
jessikim <jessica.kim17@gmail.com>
2022-12-06 15:17:44 -0600

    change landing page to render form directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import InfoForm from './components/InfoForm';
 
 import * as formService from './services/formService'
 
 function App() {
-  const navigate = useNavigate()
   const [forms, setForms] = useState([])
 
   const handleAddForm = async (formData) => {
     const newForm = await formService.create(formData)
     setForms([newForm, ...forms])
-    navigate('/form')
   }
 
   return (
     <>
       <Routes>
-        <Route path="/" />
-        <Route path="/form" element={<InfoForm handleAddForm={handleAddForm} />} />
+        <Route path="/" element={<InfoForm handleAddForm={handleAddForm} />} />
       </Routes>
     </>
   );
